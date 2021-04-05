8c4cab133a728da0ff1448baf7d8d6451c8dadd5
Jon Rosario <41496659+Jonrosario5@users.noreply.github.com>
2021-04-05 13:40:29 -0700

    Update test.js

--- test.js
+++ test.js
@@ -5,16 +5,16 @@ let dictionary = {
 }; 
 
 let conversionText = {
-    "es":{
+    es:{
       summary:"Este informe proporciona una descripción general completa de los ingresos para una empresa de SaaS B2B de muestra. La tabla principal que impulsa estas métricas contiene instantáneas diarias de todas las interacciones activas en Salesforce, comenzando en 2014 y continuando hasta el día de hoy. Esta tabla no está agregada y es rica en dimensiones y medidas, como is_month_to_month, total_seats_used y anualized_overage. Este diseño permite al liderazgo de ventas profundizar en los datos y descubrir sus propios conocimientos sin necesidad de tocar el código, lo que libera tiempo al analista para centrarse en cuestiones estratégicas y operativas más complejas.",
       revenueHeader:"Ingresos por categoría"
       
     },
-    "ru":{
+    ru:{
       summary:"В этом отчете представлен всесторонний обзор доходов для выборочной SaaS-компании B2B. Основная таблица, в которой используются эти метрики, содержит ежедневные снимки всех активных взаимодействий в Salesforce, начиная с 2014 года и по сегодняшний день. Эта таблица неагрегирована и богата измерениями и мерами, такими как is_month_to_month, total_seats_used и Annualized_overages. Такой дизайн позволяет руководству отдела продаж глубже копаться в данных и открывать собственные идеи без необходимости прикасаться к коду, тем самым высвобождая время аналитика, чтобы сосредоточиться на более сложных стратегических и операционных вопросах.",
       revenueHeader:"Выручка по категориям"
     },
-    "pt":{
+    pt:{
       summary:"Este relatório fornece uma visão geral abrangente da receita para uma amostra de empresa de SaaS B2B. A tabela principal que alimenta essas métricas contém instantâneos diários de todos os engajamentos ativos no Salesforce, começando em 2014 e continuando até os dias atuais. Esta tabela não é agregada e é rica em dimensões e medidas, como is_month_to_month, total_seats_used e annualized_overages. Esse design capacita a liderança de vendas a se aprofundar nos dados e descobrir seus próprios insights sem precisar mexer no código, liberando assim o tempo do analista para se concentrar em questões estratégicas e operacionais mais complexas.",
       revenueHeader:"Receita por categoria"
     }
