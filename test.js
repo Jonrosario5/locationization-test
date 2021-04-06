306420ee5e37980d45bbd9b29edecae216892459
Jonrosario5 <jonathan.rosario550@gmail.com>
2021-04-06 00:04:18 -0700

    updated dictionary to include more langs

--- test.js
+++ test.js
@@ -10,19 +10,146 @@ let dictionary = {
 }; 
 
 let conversionText = {
-    es:{
-      summary:"Este informe proporciona una descripción general completa de los ingresos para una empresa de SaaS B2B de muestra. La tabla principal que impulsa estas métricas contiene instantáneas diarias de todas las interacciones activas en Salesforce, comenzando en 2014 y continuando hasta el día de hoy. Esta tabla no está agregada y es rica en dimensiones y medidas, como is_month_to_month, total_seats_used y anualized_overage. Este diseño permite al liderazgo de ventas profundizar en los datos y descubrir sus propios conocimientos sin necesidad de tocar el código, lo que libera tiempo al analista para centrarse en cuestiones estratégicas y operativas más complejas.",
-      revenueHeader:"Ingresos por categoría"
-      
-    },
-    ru:{
-      summary:"В этом отчете представлен всесторонний обзор доходов для выборочной SaaS-компании B2B. Основная таблица, в которой используются эти метрики, содержит ежедневные снимки всех активных взаимодействий в Salesforce, начиная с 2014 года и по сегодняшний день. Эта таблица неагрегирована и богата измерениями и мерами, такими как is_month_to_month, total_seats_used и Annualized_overages. Такой дизайн позволяет руководству отдела продаж глубже копаться в данных и открывать собственные идеи без необходимости прикасаться к коду, тем самым высвобождая время аналитика, чтобы сосредоточиться на более сложных стратегических и операционных вопросах.",
-      revenueHeader:"Выручка по категориям"
-    },
-    pt:{
-      summary:"Este relatório fornece uma visão geral abrangente da receita para uma amostra de empresa de SaaS B2B. A tabela principal que alimenta essas métricas contém instantâneos diários de todos os engajamentos ativos no Salesforce, começando em 2014 e continuando até os dias atuais. Esta tabela não é agregada e é rica em dimensões e medidas, como is_month_to_month, total_seats_used e annualized_overages. Esse design capacita a liderança de vendas a se aprofundar nos dados e descobrir seus próprios insights sem precisar mexer no código, liberando assim o tempo do analista para se concentrar em questões estratégicas e operacionais mais complexas.",
-      revenueHeader:"Receita por categoria"
+  es:{
+    reportTitle:"Convertir idioma en el tablero",
+    chart_Configs:{
+      lang : {
+        months: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+        weekdays: [
+          'Dimanche', 'Lundi', 'Mardi', 'Mercredi',
+          'Jeudi', 'Vendredi', 'Samedi'
+          ],
+      shortMonths:["enero", 'feb.',	'marzo',	'abr.',	'mayo',	'jun.',	'jul.',	'agosto', 'set.',	'oct.',	'nov.','dic.']
     }
-  };
+    },
+    summary:"Este informe proporciona una descripción general completa de los ingresos para una empresa de SaaS B2B de muestra. La tabla principal que impulsa estas métricas contiene instantáneas diarias de todas las interacciones activas en Salesforce, comenzando en 2014 y continuando hasta el día de hoy. Esta tabla no está agregada y es rica en dimensiones y medidas, como is_month_to_month, total_seats_used y anualized_overage. Este diseño permite al liderazgo de ventas profundizar en los datos y descubrir sus propios conocimientos sin necesidad de tocar el código, lo que libera tiempo al analista para centrarse en cuestiones estratégicas y operativas más complejas.",
+    revenueHeader:"Ingresos por categoría",
+    charts:[
+      {chartTitle:"Ingresos diarios",
+      chart_id:"chart_297eb1de55f4"
+    },
+    {chartTitle:"% de ARR por segmento de cuenta",
+    chart_id:"chart_ce9bfe340c07"
+    },
+    {chartTitle:"Resumen de ingresos mensuales por categoría",
+    chart_id:"chart_c1b186093572"
+    },
+    {chartTitle:"Cuentas por acumulación de ingresos mensuales",
+    chart_id:"chart_161947d5e91b"
+    },
+    {chartTitle:"Ingresos diarios por interacción",
+    chart_id:"chart_184989c068b8"
+    }
+    ],
+    bigValue:[
+      {title:"Spot ARR (hoy)",
+      chart_id:"chart_15ae941e80da",
+      description:""
+      },
+      {title:"Clientes anuales",
+      chart_id:"chart_b96bf16471df",
+      description:""
+    },
+    {title:"Clientes totales",
+    chart_id:"chart_da568a019131",
+    description:""
+    }
+    ]
+    
+},
+rs:{
+  reportTitle:"Конвертировать язык на панели инструментов",
+  chart_Configs:{
+    lang : {
+      months: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+      weekdays: [
+        'Dimanche', 'Lundi', 'Mardi', 'Mercredi',
+        'Jeudi', 'Vendredi', 'Samedi'
+        ],
+    shortMonths:["enero", 'feb.',	'marzo',	'abr.',	'mayo',	'jun.',	'jul.',	'agosto', 'set.',	'oct.',	'nov.','dic.']
+  }
+  },
+  summary:"В этом отчете представлен всесторонний обзор доходов для выборочной SaaS-компании B2B. Основная таблица, в которой используются эти метрики, содержит ежедневные снимки всех активных взаимодействий в Salesforce, начиная с 2014 года и по сегодняшний день. Эта таблица неагрегирована и богата измерениями и мерами, такими как is_month_to_month, total_seats_used и Annualized_overages. Такой дизайн позволяет руководству отдела продаж глубже копаться в данных и открывать собственные идеи без необходимости прикасаться к коду, тем самым высвобождая время аналитика, чтобы сосредоточиться на более сложных стратегических и операционных вопросах.",
+  revenueHeader:"Выручка по категориям",
+  charts:[
+    {chartTitle:"Ежедневный доход",
+    chart_id:"chart_297eb1de55f4"
+  },
+  {chartTitle:"% ARR по сегментам аккаунта",
+  chart_id:"chart_ce9bfe340c07"
+  },
+  {chartTitle:"Ежемесячный сводный доход по категориям",
+  chart_id:"chart_c1b186093572"
+  },
+  {chartTitle:"Счета по ежемесячному накоплению доходов",
+  chart_id:"chart_161947d5e91b"
+  },
+  {chartTitle:"Ежедневный доход от взаимодействия",
+  chart_id:"chart_184989c068b8"
+  }
+  ],
+  bigValue:[
+    {title:"Spot ARR (сегодня)",
+    chart_id:"chart_15ae941e80da",
+    description:""
+    },
+    {title:"Ежегодные клиенты",
+    chart_id:"chart_b96bf16471df",
+    description:""
+  },
+  {title:"Всего клиентов",
+  chart_id:"chart_da568a019131",
+  description:""
+  }
+  ]
+  
+},
+pt:{
+  reportTitle:"Converter idioma no painel",
+  chart_Configs:{
+    lang : {
+      months: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+      weekdays: [
+        'Dimanche', 'Lundi', 'Mardi', 'Mercredi',
+        'Jeudi', 'Vendredi', 'Samedi'
+        ],
+    shortMonths:["enero", 'feb.',	'marzo',	'abr.',	'mayo',	'jun.',	'jul.',	'agosto', 'set.',	'oct.',	'nov.','dic.']
+  }
+  },
+  summary:"Este relatório fornece uma visão geral abrangente da receita para uma amostra de empresa de SaaS B2B. A tabela principal que alimenta essas métricas contém instantâneos diários de todos os engajamentos ativos no Salesforce, começando em 2014 e continuando até os dias atuais. Esta tabela não é agregada e é rica em dimensões e medidas, como is_month_to_month, total_seats_used e annualized_overages. Esse design capacita a liderança de vendas a se aprofundar nos dados e descobrir seus próprios insights sem precisar mexer no código, liberando assim o tempo do analista para se concentrar em questões estratégicas e operacionais mais complexas.",
+  revenueHeader:"Receita por categoria",
+  charts:[
+    {chartTitle:"Receita Diária",
+    chart_id:"chart_297eb1de55f4"
+  },
+  {chartTitle:"% de ARR por segmento de conta",
+  chart_id:"chart_ce9bfe340c07"
+  },
+  {chartTitle:"Acumulação da receita mensal por categoria",
+  chart_id:"chart_c1b186093572"
+  },
+  {chartTitle:"Contas por acúmulo de receita mensal",
+  chart_id:"chart_161947d5e91b"
+  },
+  {chartTitle:"Receita de engajamento diário",
+  chart_id:"chart_184989c068b8"
+  }
+  ],
+  bigValue:[
+    {title:"Spot ARR (hoje)",
+    chart_id:"chart_15ae941e80da",
+    description:""
+    },
+    {title:"Clientes Anuais",
+    chart_id:"chart_b96bf16471df",
+    description:""
+  },
+  {title:"Clientes totais",
+  chart_id:"chart_da568a019131",
+  description:""
+  }
+  ]
+  
+}
 
-
+}
